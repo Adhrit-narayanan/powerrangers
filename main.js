9ac66c92cdd7f9c7fe96ccd22a6b8dca010fd9c9
Adhrit-narayanan <84670212+Adhrit-narayanan@users.noreply.github.com>
2021-09-02 16:18:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function showranger(){
+    document.getElementById("ranger1").style.display="inline-block";
+    document.getElementById("ranger2").style.display="inline-block";
+    document.getElementById("ranger3").style.display="inline-block";
+    document.getElementById("ranger4").style.display="inline-block";
+    document.getElementById("power").style.display="inline-block";
+}
